93e4c85f8a4be91cdacdcf2ae946fac45bf0359b
Miguel Falcão <MVFalcao@users.noreply.github.com>
2019-10-01 13:44:51 -0300

    API

---
+++ EditoraAPI/EditoraAPI/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EditoraAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
