0319e56e1147ed9494e1eeb1f1f7284df99b4067
Miguel Falcão <MVFalcao@users.noreply.github.com>
2019-10-29 12:32:32 -0300

    a

--- EditoraAPI/EditoraAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace EditoraAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
