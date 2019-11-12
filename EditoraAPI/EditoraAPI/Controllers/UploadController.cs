7e913557ba1d98b912f946df39beaf82981e82c6
Miguel Falcão <42094176+MVFalcao@users.noreply.github.com>
2019-11-12 01:26:14 -0300

    ImagemUpload

---
+++ EditoraAPI/EditoraAPI/Controllers/UploadController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace EditoraAPI.Controllers
+{
+    public class UploadController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post()
+        {
+            // Ver se POST é MultiPart? 
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            // Preparar CustomMultipartFormDataStreamProvider para carga de dados
+            // (veja mais abaixo)
+
+            string fileSaveLocation = HttpContext.Current.Server.MapPath("~/EditoraAPI/EditoraAPI/Images");
+            CustomMultipartFormDataStreamProvider provider = new CustomMultipartFormDataStreamProvider(fileSaveLocation);
+            List<string> files = new List<string>();
+            try
+            {
+                // Ler conteúdo da requisição para CustomMultipartFormDataStreamProvider. 
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    files.Add(Path.GetFileName(file.LocalFileName));
+                }
+                // OK se tudo deu certo.
+                return Request.CreateResponse(HttpStatusCode.OK, files);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+    }
+
+    public class CustomMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+    {
+        public CustomMultipartFormDataStreamProvider(string path) : base(path) { }
+
+        public override string GetLocalFileName(HttpContentHeaders headers)
+        {
+            return headers.ContentDisposition.FileName.Replace("\"", string.Empty);
+        }
+    }
+}
