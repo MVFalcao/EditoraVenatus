0722afadfe121723d31b9be366add27f0bf0c26d
caiqueaugustocm <44869122+caiqueaugustocm@users.noreply.github.com>
2019-11-14 09:22:04 -0300

    Model eventos criada

---
+++ EditoraAPI/EditoraAPI/Models/Evento.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EditoraAPI.Models
+{
+    public class Evento
+    {
+        [Key] public int ID_Evento { get; set; }
+        [Required] public string Imagem_URL { get; set; }
+        [Required] public string Titulo { get; set; }
+        [Required] public string Descrição { get; set; }
+        [Required] public DateTime Data_Evento { get; set; }
+        [ForeignKey("id_escola")] public int Escola { get; set; }
+        [ForeignKey("id_livraria")] public int Livraria { get; set; }
+        [JsonIgnore] public virtual List<Cliente> Id_Autor_ou_Livraria { get; set; } //FK de tb_autor OU tb_cliente
+    }
+}
