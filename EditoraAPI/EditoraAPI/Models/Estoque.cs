dcc8411fc5413d871e11ab7f3e349b4e13a42757
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2019-11-19 13:29:55 -0300

    Revert "Merge remote-tracking branch 'origin/Caique' into Jander-patch-2"
    This reverts commit b0676b164a81eb7bf44bf3fc37b44d1a0356660a, reversing
    changes made to fd302cb99de652fc8727c45a39ad997be83638f0.

--- EditoraAPI/EditoraAPI/Models/Estoque.cs
+++ EditoraAPI/EditoraAPI/Models/Estoque.cs
@@ -11,8 +11,8 @@ namespace EditoraAPI.Models
     public class Estoque
     {
         [Key] public int ID_Estoque { get; set; }
-        [Required] public int Quantidade { get; set; }
-        [JsonIgnore] public virtual List<Livro> ID_Livro { get; set; } //FK
-        [Required] [ForeignKey("ID_Livro")] public int Livro { get; set; }
+        [Required] public int quantidade { get; set; }
+        [JsonIgnore] public virtual List<Livro> id_livro { get; set; } //FK
+        [Required] [ForeignKey("id_livro")] public int livro { get; set; }
     }
 }
--- EditoraAPI/EditoraAPI/Models/Evento.cs
+++ EditoraAPI/EditoraAPI/Models/Evento.cs
@@ -15,7 +15,7 @@ namespace EditoraAPI.Models
         [Required] public string Titulo { get; set; }
         [Required] public string Descrição { get; set; }
         [Required] public DateTime Data_Evento { get; set; }
-        [ForeignKey("Id_escola")] public int Escola { get; set; }
+        [ForeignKey("Id_escola")] public int Esc { get; set; }
         [ForeignKey("Id_livraria")] public int Livraria { get; set; }
         [JsonIgnore] public virtual List<Escola> Id_escola { get; set; }
         [JsonIgnore] public virtual List<Livraria> Id_livraria { get; set; }
