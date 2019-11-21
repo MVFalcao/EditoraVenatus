8546094d8d968a220f1047984a89610d2d88fcf6
Miguel Falcão <MVFalcao@users.noreply.github.com>
2019-11-21 13:29:02 -0300

    serto

--- EditoraAPI/EditoraAPI/Controllers/UploadController.cs
+++ EditoraAPI/EditoraAPI/Controllers/UploadController.cs
@@ -45,7 +45,7 @@ namespace EditoraAPI.Controllers
                     files.Add(Path.GetFileName(file.LocalFileName));
                 }
                 // OK se tudo deu certo.
-                var URL = Url.Content(Path.Combine(fileSaveLocation, files[0]));
+                var URL = Url.Content(Path.Combine("~/Images", files[0]));
                 return Request.CreateResponse(HttpStatusCode.OK, URL);
             }
             catch (System.Exception e)
@@ -61,7 +61,17 @@ namespace EditoraAPI.Controllers
 
         public override string GetLocalFileName(HttpContentHeaders headers)
         {
-            return headers.ContentDisposition.FileName.Replace("\"", string.Empty);
+            return alfanumericoAleatorio(40)+"_" + headers.ContentDisposition.FileName.Replace("\"", string.Empty);
+        }
+        public static string alfanumericoAleatorio(int tamanho)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, tamanho)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+            return result;
         }
     }
 }
